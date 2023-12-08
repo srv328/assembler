e688a915684253ca833e4e847172c6fc2c6e73dd
shevelev roman <75514740+srv328@users.noreply.github.com>
2023-12-09 00:52:53 +1000

    Add files via upload

---
+++ 2/HW2.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+int main()
+{
+    unsigned short a = 'a';
+    _asm {
+        mov ax, 1111111111110100b
+        xor ax, 1111111111111011b //ax = 0000000000001011b
+        or ax,  1111111111110100b // ax = 1111111111111111b
+        shr ax, 100b //ax = 0000000011111111b
+        mov a, ax
+    }
+    printf("%d", a);
+    getchar();
+}
